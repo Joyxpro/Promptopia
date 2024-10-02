247a378007357b49e19136c092367ba631d13cda
Joy Sengupta <joysengupta252005@gamil.com>
2024-10-02 15:41:23 +0530

    added the edit and delete post functions

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,18 +1,14 @@
-import Prompt from "@models/prompt"; // Import the Prompt model
-import { connectToDB } from "@utils/database"; // Import the database connection utility
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
   try {
-    await connectToDB(); // Connect to the database
+    await connectToDB();
 
-    // Fetch all prompts and populate the creator field
     const prompts = await Prompt.find({}).populate("creator");
 
-    // Return the fetched prompts as a JSON response
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    // Handle any errors that occur during the fetch
-    console.error("Error fetching prompts:", error); // Optional: Log the error for debugging
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
